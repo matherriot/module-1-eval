88ad45691b10e1f03e0bf780df021f1b545b09d7
Mathis Herriot <yidhra@tuta.io>
2023-12-10 23:17:14 +0100

    revert(scripts): :fire: console log

--- src/assets/ts/loading.ts
+++ src/assets/ts/loading.ts
@@ -1,16 +1,18 @@
-console.log("[loader] Je suis chargé !");
-const loaderBox = document.querySelector(".loader-box")
-const mainContainer = document.querySelector(".container")
-const loaderTimer = Math.round((Math.random()*4000)+2000)
+const loaderBox = document.querySelector(".loader-box");
+const mainContainer = document.querySelector(".container");
+const loaderTimer = Math.round((Math.random() * 4000) + 2000);
 
 let isOnLoadingScreen = true;
 function toggleLoadingScreen() {
-    loaderBox?.classList.toggle("loader-hidden")
-    mainContainer?.classList.toggle("hidden-container")
+  loaderBox?.classList.toggle("loader-hidden");
+  mainContainer?.classList.toggle("hidden-container");
 
-    isOnLoadingScreen = !isOnLoadingScreen
-    console.log("[loader] Changement d'état : " + isOnLoadingScreen + "\n Le délai était : " + loaderTimer + " ms");
-    return isOnLoadingScreen
+  isOnLoadingScreen = !isOnLoadingScreen;
+//   console.log(
+//     "[loader] Changement d'état : " + isOnLoadingScreen +
+//       "\n Le délai était : " + loaderTimer + " ms",
+//   );
+  return isOnLoadingScreen;
 }
 
-setTimeout(toggleLoadingScreen, (Math.random()*4000)+2000)
+setTimeout(toggleLoadingScreen, (Math.random() * 4000) + 2000);
